924d6f83ac9c55dff1911d4fac78660a4530111e
emmyone <adeyeye.eo@gmail.com>
2021-11-29 08:49:37 +0100

    I created the user interface, added withdraw and deposit functions

---
+++ atm.js
@@ -0,0 +1,51 @@
+function drawl() {
+    displaymoney.style.display= 'none';
+    displayamt.style.display= 'block';
+}
+
+function depo(){
+    displaymoney.style.display= 'none';
+    deposit.style.display= 'block';
+}
+function show2(){
+    show.innerHTML = show3.innerText + (" Deposit successful");
+}
+function posit(){
+    show.innerHTML = show4.innerHTML + ("  Deposit Successful")
+}
+
+function posit2(){
+    show.innerHTML = show5.innerHTML + (" Deposit Successful")
+}
+function posit3() {
+    show.innerHTML = show6.innerHTML + (" Deposit Successful")
+}
+function posit4(){
+    show.innerHTML = show7.innerHTML + (" Deposit Successful")
+}
+function posit5(){
+    show.innerHTML = show8.innerHTML + (" Deposit Successful")
+}
+function atm(){
+          come.innerHTML = show9.innerHTML + (" Transaction Successful")
+}
+function atm1(){
+    come.innerHTML = show10.innerHTML + (" Transaction Successful")
+}
+function atm2(){
+    come.innerHTML = show11.innerHTML + (" Transaction Successful")
+}
+function atm3(){
+    come.innerHTML = show12.innerHTML + (" Transaction Successful")
+}
+function atm4(){
+    come.innerHTML = show13.innerHTML + (" Transaction Successful")
+}
+function atm5(){
+    come.innerHTML = show13.innerHTML + (" Transaction Successful")
+}
+function well(){
+body.style.display= 'none';
+displaymoney.style.display = 'block';
+    
+}
